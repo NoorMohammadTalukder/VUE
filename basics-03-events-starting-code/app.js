1f841d6c24421a0e910f9a927d2bb75860026576
YourName <noor11809027@gmail.com>
2022-08-29 17:56:17 +0600

    event modifiers

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,24 +2,29 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name:'', 
-      
+      name: "",
+      confirmName:"",
     };
   },
-  methods:{
-    add(num){
-      this.counter+=num;
+  methods: {
+    add(num) {
+      this.counter += num;
     },
-    remove(num){
-      if(this.counter>0){
-        this.counter-=num;
+    remove(num) {
+      if (this.counter > 0) {
+        this.counter -= num;
       }
-      
     },
-    setName(e, lastName){
-      this.name=e.target.value+' '+lastName;
-    }
-  }
+    setName(e, lastName) {
+      this.name = e.target.value + " " + lastName;
+    },
+    setConfirmName() {
+      this.confirmName = this.name;
+    },
+    submitForm() {
+      alert("submitted");
+    },
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
